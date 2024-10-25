748fa96b6eb0d014faafa0d46d15c536ed861768
Maciej Bartkowiak <maciej.bartkowiak@stfc.ac.uk>
2024-10-25 09:54:21 +0100

    Introduce a slower but more stable DL_POLY converter

--- MDANSE/Src/MDANSE/Framework/Converters/DL_POLY.py
+++ MDANSE/Src/MDANSE/Framework/Converters/DL_POLY.py
@@ -28,45 +28,6 @@ from MDANSE.MolecularDynamics.Trajectory import TrajectoryWriter
 from MDANSE.MolecularDynamics.UnitCell import UnitCell
 
 
-_HISTORY_FORMAT = {}
-_HISTORY_FORMAT["2"] = {
-    "rec1": 81,
-    "rec2": 31,
-    "reci": 61,
-    "recii": 37,
-    "reciii": 37,
-    "reciv": 37,
-    "reca": 43,
-    "recb": 37,
-    "recc": 37,
-    "recd": 37,
-}
-_HISTORY_FORMAT["3"] = {
-    "rec1": 73,
-    "rec2": 73,
-    "reci": 73,
-    "recii": 73,
-    "reciii": 73,
-    "reciv": 73,
-    "reca": 73,
-    "recb": 73,
-    "recc": 73,
-    "recd": 73,
-}
-_HISTORY_FORMAT["4"] = {
-    "rec1": 73,
-    "rec2": 73,
-    "reci": 73,
-    "recii": 73,
-    "reciii": 73,
-    "reciv": 73,
-    "reca": 73,
-    "recb": 73,
-    "recc": 73,
-    "recd": 73,
-}
-
-
 class HistoryFileError(Error):
     pass
 
@@ -79,157 +40,83 @@ class HistoryFile(dict):
 
     def __init__(self, filename):
         super().__init__()
-        self["instance"] = open(filename, "rb")
-
-        version = self.determine_version()
-        self["version"] = version
-
-        lenTestLine = len(self["instance"].readline())
-
-        self["instance"].seek(0, 0)
-
-        offset = lenTestLine - _HISTORY_FORMAT[self["version"]]["rec1"]
-
-        self._headerSize = (
-            _HISTORY_FORMAT[self["version"]]["rec1"]
-            + _HISTORY_FORMAT[self["version"]]["rec2"]
-            + 2 * offset
-        )
-
-        self["instance"].read(_HISTORY_FORMAT[self["version"]]["rec1"] + offset)
-
-        data = self["instance"].read(_HISTORY_FORMAT[self["version"]]["rec2"] + offset)
-
-        self["keytrj"], self["imcon"], self["natms"] = [
-            int(v) for v in data.split()[0:3]
-        ]
-
-        if self["keytrj"] not in list(range(3)):
-            raise HistoryFileError("Invalid value for trajectory output key.")
-
-        if self["imcon"] not in list(range(4)):
-            raise HistoryFileError(
-                "Invalid value for periodic boundary conditions key."
-            )
-
-        if self["imcon"] == 0:
-            self._configHeaderSize = _HISTORY_FORMAT[self["version"]]["reci"]
-        else:
-            self._configHeaderSize = (
-                _HISTORY_FORMAT[self["version"]]["reci"]
-                + 3 * _HISTORY_FORMAT[self["version"]]["recii"]
-                + 4 * offset
-            )
-
-        self._configSize = (
-            _HISTORY_FORMAT[self["version"]]["reca"]
-            + offset
-            + (self["keytrj"] + 1) * (_HISTORY_FORMAT[self["version"]]["recb"] + offset)
-        ) * self["natms"]
-
-        self._frameSize = self._configHeaderSize + self._configSize
-
-        self["instance"].seek(0, 2)
-
-        self["n_frames"] = (
-            self["instance"].tell() - self._headerSize
-        ) / self._frameSize
-
-        self["instance"].seek(self._headerSize)
-
-        data = self["instance"].read(self._configHeaderSize).splitlines()
-
-        line = data[0].split()
-
-        self._firstStep = int(line[1])
-
-        self._timeStep = float(line[5])
-
-        self._maskStep = 3 + 3 * (self["keytrj"] + 1) + 1
-
-        if (self["version"] == "3") or (self["version"] == "4"):
-            self._maskStep += 1
-
-        self["instance"].seek(0)
+        self._dist_conversion = measure(1.0, "ang").toval("nm")
+        self._vel_conversion = measure(1.0, "ang/ps").toval("nm/ps")
+        self._grad_conversion = measure(1.0, "uma ang / ps2").toval("uma nm / ps2")
+        with open(filename, "r") as source:
+            _ = source.readline()
+            tagline = source.readline()
+            toks = tagline.split()
+            self["keytrj"], self["imcon"], self["natms"] = [int(v) for v in toks[:3]]
+            timeline = source.readline()
+            toks = timeline.split()
+            self._timeStep = float(toks[5])
+            self._firstStep = int(toks[1])
+        n_frames = 0
+        with open(filename, "r") as source:
+            for line in source:
+                if "timestep" in line:
+                    n_frames += 1
+        self["n_frames"] = n_frames
+        self["instance"] = open(filename, "r")
+        for _ in range(2):
+            self["instance"].readline()
 
     def read_step(self, step):
-        self["instance"].seek(self._headerSize + step * self._frameSize)
-
-        data = self["instance"].read(self._configHeaderSize).splitlines()
-
-        line = data[0].split()
-        currentStep = int(line[1])
+        headerline = self["instance"].readline()
+        currentStep = int(headerline.split()[1])
 
         timeStep = (currentStep - self._firstStep) * self._timeStep
         if self["imcon"] > 0:
-            cell = " ".join([i.decode("UTF-8") for i in data[1:]]).split()
-            cell = np.array(cell, dtype=np.float64)
+            cell_nums = []
+            for _ in range(3):
+                cell_nums.append(
+                    [float(x) for x in self["instance"].readline().split()]
+                )
+            cell = np.array(cell_nums, dtype=np.float64)
             cell = np.reshape(cell, (3, 3)).T
-            cell *= measure(1.0, "ang").toval("nm")
+            cell *= self._dist_conversion
         else:
             cell = None
 
-        data = np.array(self["instance"].read(self._configSize).split())
+        charges = np.empty(self["natms"])
+        positions = np.empty((self["natms"], 3))
+        lines_per_atom = 2
+        if self["keytrj"] > 0:
+            velocities = np.empty((self["natms"], 3))
+            lines_per_atom = 3
+        if self["keytrj"] > 1:
+            gradients = np.empty((self["natms"], 3))
+            lines_per_atom = 4
 
-        mask = np.ones((len(data),), dtype=bool)
-        mask[0 :: self._maskStep] = False  # atom label
-        mask[1 :: self._maskStep] = False  # atom index
-        mask[2 :: self._maskStep] = False  # atom mass
-        mask[3 :: self._maskStep] = False  # charge on atom
-        if (self["version"] == "3") or (self["version"] == "4"):
-            mask[4 :: self._maskStep] = False
+        for atom_num in range(self["natms"]):
+            for line_num in range(lines_per_atom):
+                toks = self["instance"].readline().split()
+                if line_num == 0:
+                    charges[atom_num] = float(toks[3])
+                elif line_num == 1:
+                    positions[atom_num] = [float(x) for x in toks]
+                elif line_num == 2:
+                    velocities[atom_num] = [float(x) for x in toks]
+                elif line_num == 3:
+                    gradients[atom_num] = [float(x) for x in toks]
 
-        charge_mask = np.zeros((len(data),), dtype=bool)
-        charge_mask[3 :: self._maskStep] = True
-        charge = np.array(np.compress(charge_mask, data), dtype=np.float64)
-
-        config = np.array(np.compress(mask, data), dtype=np.float64)
-
-        config = np.reshape(config, (self["natms"], 3 * (self["keytrj"] + 1)))
-
-        config[:, 0:3] *= measure(1.0, "ang").toval("nm")
-
+        positions *= self._dist_conversion
+        config = [positions]
         # Case of the velocities
-        if self["keytrj"] == 1:
-            config[:, 3:6] *= measure(1.0, "ang/ps").toval("nm/ps")
+        if self["keytrj"] > 0:
+            velocities *= self._vel_conversion
+            config.append(velocities)
 
         # Case of the velocities + gradients
-        elif self["keytrj"] == 2:
-            config[:, 3:6] *= measure(1.0, "ang/ps").toval("nm/ps")
-            config[:, 6:9] *= measure(1.0, "uma ang / ps2").toval("uma nm / ps2")
+        elif self["keytrj"] > 1:
+            gradients *= self._grad_conversion
+            config.append(gradients)
 
-        return timeStep, cell, config, charge
+        return timeStep, cell, config, charges
 
     def close(self):
         self["instance"].close()
-
-    def determine_version(self):
-        """Determine the DL_POLY version from the history file based
-        on record 1 and 2. See the DL_POLY 2, 3 and 4 manuals.
-
-        Returns
-        -------
-        str
-            The DL_POLY version.
-
-        Raises
-        ------
-        HistoryFileError
-            When the history file does not appear valid.
-        """
-        lines = self["instance"].readlines()
-        self["instance"].seek(0, 0)
-        record_1 = lines[0]
-        record_2 = lines[1]
-        if len(record_1) in [73, 74]:
-            if len(record_2.split()) == 5:
-                return "4"
-            elif len(record_2.split()) == 3:
-                return "3"
-        elif len(record_1) in [81, 82]:
-            return "2"
-
-        raise HistoryFileError("Invalid DLPOLY history file")
 
 
 class DL_POLY(Converter):
@@ -330,19 +217,19 @@ class DL_POLY(Converter):
 
         if self._historyFile["imcon"] > 0:
             conf = PeriodicRealConfiguration(
-                self._trajectory.chemical_system, config[:, 0:3], unitCell
+                self._trajectory.chemical_system, config[0], unitCell
             )
         else:
-            conf = RealConfiguration(self._trajectory.chemical_system, config[:, 0:3])
+            conf = RealConfiguration(self._trajectory.chemical_system, config[0])
 
         if self.configuration["fold"]["value"]:
             conf.fold_coordinates()
 
         if self._velocities is not None:
-            conf["velocities"] = config[:, 3:6]
+            conf["velocities"] = config[1]
 
         if self._gradients is not None:
-            conf["gradients"] = config[:, 6:9]
+            conf["gradients"] = config[2]
 
         self._trajectory.chemical_system.configuration = conf
 
