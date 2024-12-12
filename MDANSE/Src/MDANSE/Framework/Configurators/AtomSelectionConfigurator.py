f9cdfd0b5aa68d3ea3f59e418246faa0c8faaf1f
Maciej Bartkowiak <maciej.bartkowiak@stfc.ac.uk>
2024-12-12 14:05:42 +0000

    Clean up unused files

--- MDANSE/Src/MDANSE/Framework/Configurators/AtomSelectionConfigurator.py
+++ MDANSE/Src/MDANSE/Framework/Configurators/AtomSelectionConfigurator.py
@@ -13,7 +13,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-import operator
+
 from MDANSE.Framework.Configurators.IConfigurator import IConfigurator
 from MDANSE.Framework.AtomSelector import Selector
 
