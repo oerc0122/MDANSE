003612d943e53166be47b942da5e4ad9f2b4c892
Chi Cheng <chicheng85@gmail.com>
2025-01-29 15:45:11 +0000

    fixed unit tests and updated trajectory editor unit tests so they also check the properties against the original

--- MDANSE/Tests/UnitTests/TrajectoryEditor/test_editor.py
+++ MDANSE/Tests/UnitTests/TrajectoryEditor/test_editor.py
@@ -14,7 +14,7 @@ sys.setrecursionlimit(100000)
 short_traj = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     "..",
-    "Data",
+    "Converted",
     "trajectory_no_unit_cell.mdt",
 )
 
@@ -41,6 +41,11 @@ def test_editor_null():
     )
     original.close()
     changed.close()
+
+    with h5py.File(temp_name + ".mdt") as actual,  h5py.File(short_traj) as desired:
+        np.testing.assert_array_almost_equal(actual["/configuration/coordinates"], desired["/configuration/coordinates"])
+        np.testing.assert_array_almost_equal(actual["/time"], desired["/time"])
+
     os.remove(temp_name + ".mdt")
     os.remove(temp_name + ".log")
 
@@ -68,6 +73,11 @@ def test_editor_frames():
     )
     original.close()
     changed.close()
+
+    with h5py.File(temp_name + ".mdt") as actual,  h5py.File(short_traj) as desired:
+        np.testing.assert_array_almost_equal(actual["/configuration/coordinates"], desired["/configuration/coordinates"][0:501:10])
+        np.testing.assert_array_almost_equal(actual["/time"], desired["/time"][0:501:10])
+
     os.remove(temp_name + ".mdt")
     os.remove(temp_name + ".log")
 
@@ -96,6 +106,11 @@ def test_editor_atoms():
     assert changed.trajectory.chemical_system.number_of_atoms > 0
     original.close()
     changed.close()
+
+    with h5py.File(temp_name + ".mdt") as actual,  h5py.File(short_traj) as desired:
+        np.testing.assert_array_almost_equal(actual["/configuration/coordinates"], desired["/configuration/coordinates"][:,6:20,:])
+        np.testing.assert_array_almost_equal(actual["/time"], desired["/time"])
+
     os.remove(temp_name + ".mdt")
     os.remove(temp_name + ".log")
 
@@ -126,6 +141,11 @@ def test_editor_unit_cell():
     )
     original.close()
     changed.close()
+
+    with h5py.File(temp_name + ".mdt") as actual,  h5py.File(short_traj) as desired:
+        np.testing.assert_array_almost_equal(actual["/configuration/coordinates"], desired["/configuration/coordinates"])
+        np.testing.assert_array_almost_equal(actual["/time"], desired["/time"])
+
     os.remove(temp_name + ".mdt")
     os.remove(temp_name + ".log")
 
@@ -160,6 +180,11 @@ def test_editor_transmute():
     assert "B" in new_symbols
     original.close()
     changed.close()
+
+    with h5py.File(temp_name + ".mdt") as actual,  h5py.File(short_traj) as desired:
+        np.testing.assert_array_almost_equal(actual["/configuration/coordinates"], desired["/configuration/coordinates"])
+        np.testing.assert_array_almost_equal(actual["/time"], desired["/time"])
+
     os.remove(temp_name + ".mdt")
     os.remove(temp_name + ".log")
 
@@ -187,6 +212,11 @@ def test_editor_set_charges():
     assert abs(np.sum(changed.trajectory.charges(0)) - 0.2) < 1e-5
     original.close()
     changed.close()
+
+    with h5py.File(temp_name + ".mdt") as actual,  h5py.File(short_traj) as desired:
+        np.testing.assert_array_almost_equal(actual["/configuration/coordinates"], desired["/configuration/coordinates"])
+        np.testing.assert_array_almost_equal(actual["/time"], desired["/time"])
+
     os.remove(temp_name + ".mdt")
     os.remove(temp_name + ".log")
 
@@ -211,5 +241,10 @@ def test_editor_find_molecules():
     assert len(changed.trajectory.chemical_system.unique_molecules()) > 0
     original.close()
     changed.close()
+
+    with h5py.File(temp_name + ".mdt") as actual,  h5py.File(short_traj) as desired:
+        np.testing.assert_array_almost_equal(actual["/configuration/coordinates"], desired["/configuration/coordinates"])
+        np.testing.assert_array_almost_equal(actual["/time"], desired["/time"])
+
     os.remove(temp_name + ".mdt")
     os.remove(temp_name + ".log")
