046a323683dd44337c994f693edd19fdcc2bdfa7
Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
2025-09-02 13:10:14 +0100

    Update comments and refactor trajectories to ABC form (#892)
    * Update comments and refactor trajectories to ABC form
    
    * Fix issue with colour parsing

--- MDANSE/Src/MDANSE/Chemistry/ChemicalSystem.py
+++ MDANSE/Src/MDANSE/Chemistry/ChemicalSystem.py
@@ -18,6 +18,7 @@ from __future__ import annotations
 import copy
 from collections.abc import Iterable
 from functools import reduce
+from pathlib import Path
 from typing import Any, SupportsInt
 
 import h5py
@@ -320,7 +321,7 @@ class ChemicalSystem:
             new_vals = [list(padded(val, fillvalue=-1, n=size)) for val in vals]
             clusters_group.create_dataset(key, data=new_vals)
 
-    def load(self, trajectory: h5py.File | str):
+    def load(self, trajectory: h5py.File | Path | str):
         """Read the ChemicalSystem information from the trajectory.
 
         Parameters
@@ -329,7 +330,7 @@ class ChemicalSystem:
             Filename or a file object of the trajectory.
         """
         close_on_end = False
-        if isinstance(trajectory, str):
+        if isinstance(trajectory, (Path, str)):
             close_on_end = True
             source = h5py.File(trajectory)
         else:
