26316452f56a372b2c48f2c21da7b19db5cd7749
Maciej Bartkowiak <108934199+MBartkowiakSTFC@users.noreply.github.com>
2025-11-11 15:45:18 +0000

    Update the molecule finder code (#1060)
    * Update Connectivity.py, rewrite docstrings
    
    * Update Connectivity.py, rewrite docstrings
    
    * Replace the distance calculation with the one from SAS
    
    * Speed up molecule search in TrajectoryEditor
    
    * Use one function for bond finding
    
    * Add docstrings, warning on padding coordinates w/o unit cell
    
    * Fix TrajectoryEditor number of workers to 1

--- MDANSE/Src/MDANSE/MolecularDynamics/Connectivity.py
+++ MDANSE/Src/MDANSE/MolecularDynamics/Connectivity.py
@@ -15,15 +15,65 @@
 #
 from __future__ import annotations
 
+from collections.abc import Iterable
 from itertools import product
 
 import numpy as np
-from numpy.typing import NDArray
+import numpy.typing as npt
 from scipy.spatial import KDTree
 
 from MDANSE.Chemistry import ATOMS_DATABASE
 from MDANSE.Chemistry.ChemicalSystem import ChemicalSystem
+from MDANSE.MolecularDynamics.Configuration import padded_coordinates
 from MDANSE.MolecularDynamics.Trajectory import Trajectory
+
+
+def distance_calculation(
+    coordinates: npt.NDArray[float], max_distance: float, worker_limit: int = -1
+) -> tuple[npt.NDArray[float], npt.NDArray[int], npt.NDArray[int], npt.NDArray[float]]:
+    """Calculate distances between atoms in the input array.
+
+    KDTree is used to calculate distances, and only distances within
+    the max_distance cutoff are considered.
+
+    This calculation is typically used for detecting chemical bonds between
+    atoms based on interatomic distances.
+
+    Parameters
+    ----------
+    coordinates : npt.NDArray[float]
+        An (N,3) array of atom coordinates.
+    max_distance : float
+        The largest distance to be considered in the calculation.
+    worker_limit : int, optional
+        Number of CPU cores to use, where -1 uses all available cores, by default -1
+
+    Returns
+    -------
+    tuple[npt.NDArray[float], npt.NDArray[int], npt.NDArray[int], npt.NDArray[float]]
+        Distance array, Indices of first atom, Indices of second atom, Coordinate difference array.
+    """
+    tree = KDTree(coordinates)
+    contacts = tree.query_ball_point(coordinates, max_distance, workers=worker_limit)
+    n_dists = sum([len(i) for i in contacts])
+
+    js = np.zeros(n_dists, dtype=int)
+    ks = np.zeros(n_dists, dtype=int)
+    start = 0
+    for i, idxs in enumerate(contacts):
+        n_idxs = len(idxs)
+        if n_idxs == 0:
+            continue
+        js[start : start + n_idxs] = i
+        ks[start : start + n_idxs] = idxs
+        start += n_idxs
+
+    mask = js < ks
+    js = js[mask]
+    ks = ks[mask]
+    diff = coordinates[js] - coordinates[ks]
+    dist = np.sum(diff * diff, axis=1)
+    return dist, js, ks, diff
 
 
 class Connectivity:
@@ -33,36 +83,23 @@ class Connectivity:
 
     def __init__(
         self,
-        *args,
         trajectory: Trajectory,
         selection: list[int] | None = None,
-        **kwargs,
+        parallel_workers: int | None = None,
     ):
-        self._chemical_system = trajectory.chemical_system
+        self.worker_limit = parallel_workers if parallel_workers else -1
         self._input_trajectory = trajectory
-        self._selection = selection
-        self._frames = trajectory
-        self._unit_cell = self._input_trajectory.configuration(0)
+        self._selection = selection or trajectory._selection or trajectory.atom_indices
         self._periodic = self._input_trajectory.configuration(0).is_periodic
-        self.check_composition(self._chemical_system)
-        self._bonds = None
-        self._bond_mapping = None
-        self._unique_bonds = None
+        self.check_composition()
+        self._unique_bonds = []
         self._translation_vectors = {}
 
-    def check_composition(self, chemical: ChemicalSystem):
-        """Takes the information about the system from an instance
-        of the ChemicalSystem class.
-
-        Arguments:
-            chemical -- ChemicalSystem instance connected to the trajectory.
-        """
-        if self._selection is not None:
-            atom_elements = [
-                self._chemical_system.atom_list[index] for index in self._selection
-            ]
-        else:
-            atom_elements = self._chemical_system.atom_list
+    def check_composition(self):
+        """Get atom types and radii from the trajectory."""
+        atom_elements = list(
+            np.array(self._input_trajectory.atom_types)[self._selection]
+        )
         unique_elements = np.unique(atom_elements)
         radii = {
             element: ATOMS_DATABASE.get_atom_property(element, "covalent_radius")
@@ -71,97 +108,60 @@ class Connectivity:
         self._elements = atom_elements
         self._unique_elements = unique_elements
         self._radii = radii
+        self.max_distance = 2 * np.max(list(radii.values())) + 0.04
 
-    def get_coordinates(self, frame_number: int = 0) -> NDArray[np.float64]:
-        """Returns a numpy array of the atom positions at the specified
-        simulation step (frame).
+    def total_distances_sq(
+        self, frame_index: int = 0, max_distance: float | None = None
+    ) -> dict[tuple[int, int], float]:
+        """Calculate squared distances between pairs of atoms within the specified limit.
 
-        Keyword Arguments:
-            frame_number -- number of the trajectory frame (default: {0})
+        Parameters
+        ----------
+        frame_index : int, optional
+            Trajectory frame for which the distances are calculated, by default 0
+        max_distance : float, optional
+            Distance limit for bond calculation, in nm, by default None
 
-        Returns:
-            NDArray -- an (N,3) array with the positions of N atoms
+        Returns
+        -------
+        dict[tuple[int, int], float]
+            Dictionary of {(atom index 1, atom_index 2) : distance} pairs.
         """
-        if frame_number < 0 or frame_number >= len(self._frames):
-            return None
-        if self._selection is not None:
-            return self._frames.coordinates(frame_number)[self._selection, :]
-        else:
-            return self._frames.coordinates(frame_number)
-
-    def internal_distances(
-        self, frame_number: int = 0, max_distance: float = 0.2
-    ) -> NDArray[np.float64]:
-        """Calculates an (N,N) array of interatomic distances SQUARED within
-        the simulation box. If there are no periodic boundary conditions,
-        the returned array contains ALL the distances in the system.
-
-        Keyword Arguments:
-            frame_number -- number of the trajectory frame at which to calculate
-                distances (default: {0})
-
-        Returns:
-            NDArray -- an (N,N) array of squared distances between all the atom pairs.
-        """
-        coordinates = self.get_coordinates(frame_number=frame_number)
-        if coordinates is None:
-            return None
-        tree = KDTree(coordinates)
-        return tree.sparse_distance_matrix(tree, max_distance=max_distance)
-
-    def periodic_distances(
-        self, frame_number: int = 0, max_distance: float = 0.2
-    ) -> NDArray[np.float64]:
-        """Calculates the distances between the atoms in the simulation box
-        and a copy of the simulation box translated by a unit cell vector.
-        Only needed if the simulation was run with periodic boundary conditions.
-
-        Keyword Arguments:
-            frame_number -- number of the trajectory frame at which to calculate
-                distances (default: {0})
-
-        Returns:
-            None, if an invalid frame number has been given as input
-
-        Yields:
-            NDArray -- an (N,N) array of squared distances between all the atom pairs,
-                one for each combination of the unit cell vectors.
-        """
-        unit_cell = self._input_trajectory.configuration(frame_number).unit_cell
-        vector_a, vector_b, vector_c = (
-            unit_cell.a_vector,
-            unit_cell.b_vector,
-            unit_cell.c_vector,
+        coordinates, atom_indices = padded_coordinates(
+            self._input_trajectory.coordinates(frame_index, self._selection),
+            self._input_trajectory.unit_cell(frame_index),
+            1.1 * max_distance,
         )
-        coordinates = self.get_coordinates(frame_number=frame_number)
-        if coordinates is None:
-            return None
-        tree1 = KDTree(coordinates)
-        for num, shift in enumerate(product([-1, 0, 1], repeat=3)):
-            if np.allclose(shift, [0, 0, 0]):
-                continue
-            self._translation_vectors[num] = shift
-            offset = shift[0] * vector_a + shift[1] * vector_b + shift[2] * vector_c
-            tree2 = KDTree(coordinates + offset.reshape((1, 3)))
-            yield num, tree1.sparse_distance_matrix(tree2, max_distance=max_distance)
+        dist, js, ks, diff = distance_calculation(
+            coordinates,
+            max_distance or self.max_distance,
+            worker_limit=self.worker_limit,
+        )
+
+        return {
+            (atom_indices[js[common_index]], atom_indices[ks[common_index]]): dist[
+                common_index
+            ]
+            for common_index in range(len(diff))
+        }
 
     def find_bonds(self, frames: list[int] | None = None, tolerance: float = 0.04):
-        """Checks several frames of the trajectory for the presence of atom pairs
+        """Find bonds in the trajectory based on interatomic distances.
+
+        Checks several frames of the trajectory for the presence of atom pairs
         close enough to each other to form chemical bonds. The detected bonds
         are stored internally.
 
         Parameters
         ----------
-        frames : Optional[List[int]]
-            A list of specific trajectory frames at which to check the bond
-            length. Optional (default: {None})
-        tolerance : float
-            A float constant specifying the tolerance of bond length used
-            in bond detection. A bond between two atoms is defined as the
-            sum of their covalent radii plus the tolerance in nm.
+        frames : list[int] | None, optional
+            Frames for which to check if bonds exist, by default None
+        tolerance : float, optional
+            Bond length is allowed to exceed the sum of radii by this much, by default 0.04
         """
         if frames is None:
-            samples = [len(self._frames) // denom for denom in [2, 3, 4, 5, 6, 7]]
+            max_frame = len(self._input_trajectory)
+            samples = [0, max_frame - 1]
         else:
             samples = frames
         samples = list(np.unique(samples))
@@ -170,41 +170,20 @@ class Connectivity:
             pair: (self._radii[pair[0]] + self._radii[pair[1]]) + tolerance
             for pair in pairs
         }
+        maxbonds_sq = {key: value**2 for key, value in maxbonds.items()}
         total_max_length = max(maxbonds.values())
-        for _, frame_number in enumerate(samples):
-            distances = self.internal_distances(
-                frame_number=frame_number, max_distance=total_max_length
-            )
-            if self._periodic:
-                for _, dist in self.periodic_distances(
-                    frame_number=frame_number, max_distance=total_max_length
-                ):
-                    for key, value in dist.items():
-                        if key not in distances:
-                            distances[key] = value
         bonds = []
-        bond_mapping = {atom_number: [] for atom_number in range(len(self._elements))}
-        for key, value in distances.items():
-            if key[0] == key[1]:
-                continue
-            element_pair = (self._elements[key[0]], self._elements[key[1]])
-            if value > maxbonds[element_pair]:
-                continue
-            bonds.append(key)
-            bond_mapping[key[0]].append(key[1])
-        self._bonds = bonds
-        self._bond_mapping = bond_mapping
+        for frame_index in samples:
+            distances_sq = self.total_distances_sq(
+                frame_index=frame_index, max_distance=total_max_length
+            )
+            for key, value in distances_sq.items():
+                element_pair = (self._elements[key[0]], self._elements[key[1]])
+                if value > maxbonds_sq[element_pair]:
+                    continue
+                bonds.append(key)
         self._unique_bonds = np.unique(np.sort(bonds, axis=1), axis=0)
 
     def add_bond_information(self, new_chemical_system: ChemicalSystem):
         new_chemical_system.add_bonds(self._unique_bonds)
         new_chemical_system.find_clusters_from_bonds()
-
-    def add_point(self, index: int, point: np.ndarray, radius: float) -> bool:
-        return True
-
-    def find_collisions(self, tolerance: float) -> dict:
-        return {}
-
-    def get_neighbour(self, point: np.ndarray) -> int:
-        return 0
